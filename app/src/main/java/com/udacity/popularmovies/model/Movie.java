05ae338c6999e042404bd7c9eb419f33aaf96438
barnakaracsony <git8.XyZ>
2019-05-30 20:52:09 +0200

    MoviesParserTest passes

---
+++ app/src/main/java/com/udacity/popularmovies/model/Movie.java
@@ -0,0 +1,50 @@
+package com.udacity.popularmovies.model;
+
+public class Movie {
+
+    private String title;
+    private String posterPath;
+    private String plotSynopsis;
+    private double rating;
+    private String releaseDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+
+    public void setPlotSynopsis(String plotSynopsis) {
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
---
+++ app/src/main/java/com/udacity/popularmovies/utils/json/MoviesParser.java
@@ -0,0 +1,45 @@
+package com.udacity.popularmovies.utils.json;
+
+import com.udacity.popularmovies.model.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoviesParser {
+
+    private static final String TITLE = "title";
+    private static final String POSTER_PATH = "poster_path";
+    private static final String PLOT_SYNOPSIS = "overview";
+    private static final String USER_RATING = "vote_average";
+    private static final String RELEASE_DATE = "release_date";
+
+    public static List<Movie> parse(String moviesJSON) throws JSONException {
+        JSONObject page1 = new JSONObject(moviesJSON);
+        JSONArray results = page1.getJSONArray("results");
+        return parseResults(results);
+    }
+
+    private static List<Movie> parseResults(JSONArray results) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < results.length(); i++) {
+            JSONObject movieJSON = results.getJSONObject(i);
+            Movie movie = parseMovie(movieJSON);
+            movies.add(movie);
+        }
+        return movies;
+    }
+
+    private static Movie parseMovie(JSONObject movieJSON) throws JSONException {
+        Movie movie = new Movie();
+        movie.setTitle(movieJSON.getString(TITLE));
+        movie.setPosterPath(movieJSON.getString(POSTER_PATH));
+        movie.setPlotSynopsis(movieJSON.getString(PLOT_SYNOPSIS));
+        movie.setRating(movieJSON.getDouble(USER_RATING));
+        movie.setReleaseDate(movieJSON.getString(RELEASE_DATE));
+        return movie;
+    }
+}
